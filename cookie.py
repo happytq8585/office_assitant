940305f4dc0d6d30a3e58d194a7d46a4d00b25dc
ygb <ygb@localhost.localdomain>
2017-05-16 16:48:29 +0800

    test for html+css

--- cookie.py
+++ cookie.py
@@ -39,16 +39,22 @@ class LogoutHandler(BaseHandler):
         self.clear_cookie("username")
         self.redirect("/")
 
+class TestHandler(BaseHandler):
+    def get(self):
+        self.render('test.html')
+
 if __name__ == "__main__":
     tornado.options.parse_command_line()
     settings = {
         "template_path": os.path.join(os.path.dirname(__file__), "templates"),
+        "static_path": os.path.join(os.path.dirname(__file__), "static"),
         "cookie_secret": "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
         "xsrf_cookies": True,
         "login_url": "/login" }
     application = tornado.web.Application([ (r'/', WelcomeHandler),
                                             (r'/login', LoginHandler),
-                                            (r'/logout', LogoutHandler)
+                                            (r'/logout', LogoutHandler),
+                                            (r'/test', TestHandler)
                                             ], **settings)
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
